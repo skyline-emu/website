9971459a8cf7c74a7e9a27ea58577621b2cb8a48
msoham123 <msoham123@gmail.com>
2022-01-18 15:53:03 -0800

    Import requirements for dark theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,31 @@
-import * as React from "react"
+import React, { useState } from "react"
 import Navbar from "../components/Navbar"
 import 'tw-elements';
 import Landing from "../components/Landing";
+import { Helmet } from "react-helmet"
 
-// markup
+
+
 const IndexPage = () => {
+
+
+  const [isDark, setIsDark] = useState(true)
+
+  const buttonHandler = () => {
+    setIsDark(current => !current)
+  }
+
   return (
     <main>
+
+      <Helmet htmlAttributes={{ lang: 'en' }}>
+        <meta charSet="utf-8" />
+        <meta name="description" content=""/>
+        <title>Skyline</title>
+      </Helmet>
+
       <Navbar />
-      <Landing/>
+      <Landing />
     </main>
   )
 }
