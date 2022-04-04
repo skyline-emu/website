274bb0d86230e1467a80a26e8eb1a9fe3e8ab8c6
msoham123 <msoham123@gmail.com>
2022-04-03 18:16:04 -0700

    Updated learn more button with proper hover color and font

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -2,6 +2,7 @@ import React from "react"
 import vulkan from "../images/vulkan.png"
 import vulkan_white from "../images/vulkan-white.png"
 import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "react-scroll"
 
 const Landing = () => {
     return (
@@ -38,20 +39,20 @@ const Landing = () => {
                             <button className="px-5 py-2 ml-2 font-bold text-blue-500 uppercase transition-colors duration-700 transform border-2 border-gray-600 rounded-lg lg:text-lg lg:ml-3 hover:bg-white">Learn More</button> */}
                             <div class="flex space-x-2 justify-center">
                                 <div>
-                                    <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className="flex px-8 py-3 text-sm font-medium leading-normal text-white uppercase transition duration-150 ease-in-out bg-blue-500 rounded-lg shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg align-center font-roboto">
+                                    <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className="flex px-8 py-3 text-sm font-medium leading-normal text-white uppercase transition duration-150 ease-in-out bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg align-center font-roboto">
                                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download"
                                             className="w-4 mr-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                             <path fill="currentColor"
                                                 d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z">
                                             </path>
                                         </svg>
-                                        Download
+                                        <Link activeClass="active" to="download" spy={true} smooth={true} offset={-200} duration={500}>Download</Link>
                                     </button>
                                 </div>
                             </div>
                             <div class="flex space-x-2 justify-center ml-3">
-                                <button data-mdb-ripple="true" data-mdb-ripple-color="light" type="button" class="font-roboto inline-block px-8 py-3 border-2 border-blue-600 text-blue-600 font-medium text-sm leading-tight uppercase rounded-lg hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">
-                                    Learn More
+                                <button data-mdb-ripple="true" data-mdb-ripple-color="light" type="button" class="font-roboto inline-block px-8 py-3 border-2 border-blue-600 text-blue-600 font-medium text-sm leading-tight uppercase rounded-lg  focus:outline-none focus:ring-0 transition duration-150 ease-in-out hover:bg-blue-600 hover:text-white hover:border-blue-600 font-roboto">
+                                    <Link activeClass="active" to="about" spy={true} smooth={true} offset={-200} duration={500}>Learn More</Link>
                                 </button>
                             </div>
                         </ul>
