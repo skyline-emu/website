3bf1b717d9c28559ece9dde907f250ce01bbda2b
msoham123 <msoham123@gmail.com>
2022-06-01 15:54:16 -0700

    Add license jsdoc to all js files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright © 2022 Skyline Team and Contributors (https://github.com/skyline-emu/)
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 module.exports = {
     siteMetadata: {
       siteUrl: "https://skyline-emu.one/",
--- robots-txt.config.js
+++ robots-txt.config.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright © 2022 Skyline Team and Contributors (https://github.com/skyline-emu/)
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 module.exports = {
     host: 'https://skyline-emu.one/',
     policy: [{userAgent: '*', disallow: ['']}]
