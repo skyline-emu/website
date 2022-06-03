9e10715d652cc8ad73504960804a18a19ffec301
Barış DEMİRCİ <demirci.baris38@gmail.com>
2022-06-03 11:16:49 +0000

    Divide members into teams
    Divide team members according to the teams they are in. Also, the code automatically adds the team role to the member.

--- src/config/team.js
+++ src/config/team.js
@@ -4,65 +4,66 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+const lead_developers = [
+    {
+        name: "Mark",
+        avatar: "https://avatars.githubusercontent.com/u/17637537",
+        github: "https://github.com/PixelyIon",
+    },
+];
+
+const developers = [
+    {
+        name: "Billy",
+        avatar: "https://avatars.githubusercontent.com/u/46304689",
+        github: "https://github.com/bylaws",
+    },
+    {
+        name: "Lynx",
+        avatar: "https://avatars.githubusercontent.com/u/37104290",
+        github: "https://github.com/lynxnb",
+    },
+    {
+        name: "MC",
+        avatar: "https://avatars.githubusercontent.com/u/63514060",
+        github: "https://github.com/MCredstoner2004",
+    },
+];
+
+const web_developers = [
+    {
+        name: "Ducc",
+        avatar: "https://avatars.githubusercontent.com/u/51520568",
+        github: "https://github.com/msoham123",
+    },
+];
+
+const community_managers = [
+    {
+        name: "GalladeGuy",
+        avatar: "https://avatars.githubusercontent.com/u/15641825",
+        github: "https://github.com/GalladeGuy",
+    },
+    {
+        name: "King",
+        avatar: "https://avatars.githubusercontent.com/u/73195869",
+        github: "https://github.com/MrKing202981",
+    },
+    {
+        name: "WillFaustCuber",
+        avatar: "https://avatars.githubusercontent.com/u/39139570",
+        github: "https://github.com/willfaust",
+    },
+    {
+        name: "Ivar",
+        avatar: "https://avatars.githubusercontent.com/u/41924494",
+        github: "https://github.com/IvarWithoutBones",
+    },
+];
+
 export const team = [
-    {
-        name: "Mark", 
-        avatar: "https://avatars.githubusercontent.com/u/17637537", 
-        github: "https://github.com/PixelyIon", 
-        title: "Lead Developer",
-    },
-    {
-        name: "Billy", 
-        avatar: "https://avatars.githubusercontent.com/u/46304689", 
-        github: "https://github.com/bylaws", 
-        title: "Developer",
-    },
-    {
-        name: "Lynx", 
-        avatar: "https://avatars.githubusercontent.com/u/37104290", 
-        github: "https://github.com/lynxnb", 
-        title: "Developer",
-    },
-    {
-        name: "MC", 
-        avatar: "https://avatars.githubusercontent.com/u/63514060", 
-        github: "https://github.com/MCredstoner2004", 
-        title: "Developer",
-    },
-    {
-        name: "Ducc", 
-        avatar: "https://avatars.githubusercontent.com/u/51520568", 
-        github: "https://github.com/msoham123", 
-        title: "Web Developer",
-    },
-    {
-        name: "Barış", 
-        avatar: "https://avatars.githubusercontent.com/u/35371155", 
-        github: "https://github.com/barbarbar338", 
-        title: "Web Developer",
-    },
-    {
-        name: "GalladeGuy", 
-        avatar: "https://avatars.githubusercontent.com/u/15641825", 
-        github: "https://github.com/GalladeGuy", 
-        title: "Community Manager",
-    },
-    {
-        name: "King", 
-        avatar: "https://avatars.githubusercontent.com/u/73195869", 
-        github: "https://github.com/MrKing202981", 
-        title: "Community Manager",
-    },
-    {
-        name: "WillFaustCuber", 
-        avatar: "https://avatars.githubusercontent.com/u/39139570", 
-        github: "https://github.com/willfaust", 
-        title: "Community Manager",
-    },
-    {
-        name: "Ivar", 
-        avatar: "https://avatars.githubusercontent.com/u/41924494", 
-        github: "https://github.com/IvarWithoutBones", 
-        title: "Community Manager",
-    },
+    ...lead_developers.map(i => ({...i, title: "Lead Developer"})),
+    ...developers.map(i => ({...i, title: "Developer"})),
+    ...web_developers.map(i => ({...i, title: "Web Developer"})),
+    ...community_managers.map(i => ({...i, title: "Community Manager"})),
 ];
