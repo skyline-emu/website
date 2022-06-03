01090ea29db0a29eb2444e54396a39e035891407
Barış DEMİRCİ <demirci.baris38@gmail.com>
2022-06-03 13:43:17 +0000

    Fix theme change button going out of bounds
    The theme change button was running out of bounds due to the inconvenience caused by the size of the Vulkan image, so the Vulkan image was enclosed in a fixed div.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import About from "../sections/About"
 import Team from "../sections/Team"
 import { Helmet } from "react-helmet"
 import { useLocalStorage } from "react-use"
+import { FaMoon, FaSun } from "react-icons/fa"
 
 // import Download from "../sections/Download"
 
@@ -30,30 +31,16 @@ const IndexPage = () => {
         <meta name="keywords" content="Emulator, Nintendo Switch™, Skyline, Android" />
         <meta name="author" content="Skyline Team" />
       </Helmet>
+
       <div className="fixed z-50 bottom-10 right-10 ">
         <div className="flex space-x-2 justify-center">
           <div>
             <button onClick={buttonHandler} type="button" className="inline-block rounded-full bg-blue-500 text-white leading-normal uppercase shadow-md hover:bg-blue-600 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-12 h-12">
               <div className="flex justify-center">
                 {isDark ?
-                  <svg
-                    className="w-5 h-5 block"
-                    fill="white"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
-                    />
-                  </svg> :
-                  <svg
-                    className="w-5 h-5 block"
-                    fill="white"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                    />
-                  </svg>
+                  <FaMoon className="w-5 h-5 block" />
+                  :
+                  <FaSun className="w-5 h-5 block" />
                 }
               </div>
             </button>
