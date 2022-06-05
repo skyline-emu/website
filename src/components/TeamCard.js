9346ee3e153eb7839151a105d77bae0546de068e
PixelyIon <pixelyion@protonmail.com>
2022-06-05 15:16:29 +0530

    Remove shadows around `TeamCard`
    The shadows around `TeamCard` while providing a sense of depth are ultimately inconsistent with the rest of the design, we need a more cohesive and compact design for this in the future.

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -9,7 +9,7 @@ import CustomLink from "./CustomLink";
 
 const TeamCard = ({ name, avatar, github, title }) => (
     <div>
-        <div className="block bg-white rounded-lg shadow-lg dark:bg-zinc-900 dark:text-white">
+        <div className="block bg-stone-200 rounded-lg dark:bg-zinc-900 dark:text-white">
             <div className="h-20 overflow-hidden rounded-t-lg"></div>
             <div className="w-20 mx-auto -mt-12 overflow-hidden bg-white border-2 border-white rounded-full">
                 <img alt={name} src={avatar}/>
