ea671f51626f023f4e5dd4d1479e32091be0d1e1
msoham123 <msoham123@gmail.com>
2022-06-08 13:38:47 -0700

    Added section title and description to match rest of site

--- src/pages/download.js
+++ src/pages/download.js
@@ -13,28 +13,29 @@ import useSWR from "swr"
 import config from "../config";
 
 const IndexPage = () => {
-  const {data, isValidating} = useSWR("get-builds", async () => {
+  const { data, isValidating } = useSWR("get-builds", async () => {
     const res = await axios.get(config.builds_api_url);
     console.log(res.status, res.data);
     return res.data;
   });
-  
+
   return (
     <Layout title="Download Skyline - Nintendo Switch Emulator">
-      <div className="w-full pb-0 md:pb-20 min-h-screen dark:bg-darkBackground">
+      <div className="w-full min-h-screen pb-0 md:pb-20 dark:bg-darkBackground">
         <div className="flex items-center justify-between">
           <div className="px-8 py-12 mx-auto md:py-18 max-w-page">
-            <h1
-              className="text-xl items-center font-bold  md:text-3xl dark:text-white"
-            >
-              Skyline Builds
-            </h1>
+            <div className="pt-4 mx-auto text-center md:max-w-xl lg:max-w-3xl">
+              <h3 className="mb-6 text-3xl font-bold text-black dark:text-white">Builds</h3>
+              <p className="pb-2 mb-6 font-semibold text-blue-500 md:mb-12 md:pb-0">
+                Click on Download to download the apk file for the emulator.
+              </p>
+            </div>
             {isValidating ? <Preloader /> : data.map((build, idx) => {
               const split = build.commit.message.split("\n\n");
               const title = split.shift();
               const description = split.join("\n\n");
               return (
-                <BuildCard 
+                <BuildCard
                   key={idx}
                   createdAt={build.createdAt}
                   title={title}
@@ -51,7 +52,7 @@ const IndexPage = () => {
           </div>
         </div>
       </div>
-    </Layout> 
+    </Layout>
   )
 }
 
