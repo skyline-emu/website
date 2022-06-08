12aa5db72c83d3a6c52aac59428d0c6b1c95a601
Barış DEMİRCİ <demirci.baris38@gmail.com>
2022-06-08 08:21:34 +0000

    Create preloader component
    A preloader component has been created to appear while the build data is being loaded.

---
+++ src/components/Preloader.js
@@ -0,0 +1,16 @@
+/**
+ * @license
+ * Copyright © 2022 Skyline Team and Contributors (https://github.com/skyline-emu/)
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import React from "react";
+
+const Preloader = () => (
+    <div className="preloader-container">
+        <div className="preloader-1"></div>
+        <div className="preloader-2"></div>
+    </div>
+);
+
+export default Preloader;
