ffa14b7362703efd50cb72878c0f939f2faf8bdf
msoham123 <msoham123@gmail.com>
2022-06-18 11:54:37 -0700

    Fix button overflow on build card

--- src/components/BuildCard.js
+++ src/components/BuildCard.js
@@ -13,7 +13,7 @@ import { marked } from "marked";
 
 const BuildCard = ({ createdAt, title, description, download_url, github_url, branch, id, hash, number }) => (
     <div className="mt-6">
-        <div className="max-w-7xl px-5 py-6 mx-auto bg-stone-50 dark:bg-zinc-900 rounded">
+        <div className="px-5 py-6 mx-auto rounded max-w-7xl bg-stone-50 dark:bg-zinc-900">
             <div className="flex items-center justify-between">
                 <span className="font-light text-gray-600 dark:text-white">{DateTime.fromISO(createdAt).toLocaleString()}</span>
             </div>
@@ -29,17 +29,17 @@ const BuildCard = ({ createdAt, title, description, download_url, github_url, br
                 }}
             />
             <div className="mt-5 text-gray-800 dark:text-gray-200">
-                <div className="truncate max-w-full">
+                <div className="max-w-full truncate">
                     <strong>Branch:</strong> <code>{branch}</code>
                 </div>
-                <div className="truncate max-w-full">
+                <div className="max-w-full truncate">
                     <strong>Commit Hash:</strong> <code>{hash}</code>
                 </div>
-                <div className="truncate max-w-full">
+                <div className="max-w-full truncate">
                     <strong>Run ID:</strong> {id}
                 </div>
             </div>
-            <div className="flex items-center mt-4 gap-3">
+            <div className="flex flex-col items-center gap-3 mt-4 md:flex-row">
                 <Button href={download_url} icon={<FaDownload />} text="Download" />
                 <Button href={github_url} text="View On Github" />
             </div>
