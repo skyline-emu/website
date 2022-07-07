ca0f7f847f6e53dcc9ecb121f73b36c1d6ad6901
msoham123 <msoham123@gmail.com>
2022-07-07 12:26:23 -0700

    Replace imported avatars with github cdn links

--- src/config/team.js
+++ src/config/team.js
@@ -4,21 +4,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
- import mark from "../images/team/mark.webp"
- import billy from "../images/team/billy.webp"
- import lynx from "../images/team/lynx.webp"
- import mc from "../images/team/mc.webp"
- import ducc from "../images/team/ducc.webp"
- import glade from "../images/team/glade.webp"
- import king from "../images/team/king.webp"
- import will from "../images/team/will.webp"
- import ivar from "../images/team/ivar.webp"
-
 
 const lead_developers = [
     {
         name: "Mark",
-        avatar: mark,
+        avatar: "https://cdn.discordapp.com/avatars/180662388301889536/88aa9e8f2a9098062d80a8c22f14308a.webp?size=100",
         github: "https://github.com/PixelyIon",
     },
 ];
@@ -26,25 +16,25 @@ const lead_developers = [
 const developers = [
     {
         name: "Billy",
-        avatar: billy,
+        avatar: "https://cdn.discordapp.com/avatars/463327301589794826/43d6ae6ac69648af4ce48e6ce1c66933.webp?size=100",
         github: "https://github.com/bylaws",
     },
     {
         name: "Lynx",
-        avatar: lynx,
+        avatar: "https://cdn.discordapp.com/avatars/200204941300727808/d9f6e7511ec9a034fa6b804e8f023505.webp?size=100",
         github: "https://github.com/lynxnb",
     },
     {
         name: "MC",
-        avatar: mc,
+        avatar: "https://cdn.discordapp.com/avatars/508468419268706305/771b7acff39fc9ee605d9adda6140789.webp?size=100",
         github: "https://github.com/MCredstoner2004",
     },
 ];
 
 const web_developers = [
     {
-        name: "Ducc",
-        avatar: ducc,
+        name: "ducc",
+        avatar: "https://cdn.discordapp.com/avatars/374688019472711701/46bcc66a4abe89ab613de4d62a7a46a8.webp?size=100",
         github: "https://github.com/msoham123",
     },
 ];
@@ -52,22 +42,22 @@ const web_developers = [
 const community_managers = [
     {
         name: "GalladeGuy",
-        avatar: glade,
+        avatar: "https://cdn.discordapp.com/avatars/171242584507678720/fbe610906bd63f2aceb3afd29d28bdee.webp?size=100",
         github: "https://github.com/GalladeGuy",
     },
     {
         name: "King",
-        avatar: king,
+        avatar: "https://cdn.discordapp.com/guilds/545842171459272705/users/707984911616704604/avatars/34cb914f7199528e3c1a53f77ceaa5c8.webp?size=100",
         github: "https://github.com/MrKing202981",
     },
     {
         name: "WillFaustCuber",
-        avatar: will,
+        avatar: "https://cdn.discordapp.com/avatars/451407331234807830/3056ed1f40d915b7e9c5587960fa5c51.webp?size=100",
         github: "https://github.com/willfaust",
     },
     {
         name: "Ivar",
-        avatar: ivar,
+        avatar: "https://cdn.discordapp.com/avatars/290398986085597184/871fe950051d98b88195cfe164fcf1b3.webp?size=100",
         github: "https://github.com/IvarWithoutBones",
     },
 ];
