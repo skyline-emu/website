fee1f6b2051a44b29d84245057f1e74691bbe28e
msoham123 <msoham123@gmail.com>
2022-07-11 12:12:54 -0700

    Revert Avatar srcs to Github CDN

--- src/config/team.js
+++ src/config/team.js
@@ -8,7 +8,7 @@
 const lead_developers = [
     {
         name: "Mark",
-        avatar: "https://cdn.discordapp.com/avatars/180662388301889536/88aa9e8f2a9098062d80a8c22f14308a.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/17637537",
         github: "https://github.com/PixelyIon",
     },
 ];
@@ -16,17 +16,17 @@ const lead_developers = [
 const developers = [
     {
         name: "Billy",
-        avatar: "https://cdn.discordapp.com/avatars/463327301589794826/43d6ae6ac69648af4ce48e6ce1c66933.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/46304689",
         github: "https://github.com/bylaws",
     },
     {
         name: "Lynx",
-        avatar: "https://cdn.discordapp.com/avatars/200204941300727808/d9f6e7511ec9a034fa6b804e8f023505.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/37104290",
         github: "https://github.com/lynxnb",
     },
     {
         name: "MC",
-        avatar: "https://cdn.discordapp.com/avatars/508468419268706305/771b7acff39fc9ee605d9adda6140789.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/63514060",
         github: "https://github.com/MCredstoner2004",
     },
 ];
@@ -34,7 +34,7 @@ const developers = [
 const web_developers = [
     {
         name: "ducc",
-        avatar: "https://cdn.discordapp.com/avatars/374688019472711701/46bcc66a4abe89ab613de4d62a7a46a8.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/51520568",
         github: "https://github.com/msoham123",
     }
 ];
@@ -50,22 +50,22 @@ const bot_developers = [
 const community_managers = [
     {
         name: "GalladeGuy",
-        avatar: "https://cdn.discordapp.com/avatars/171242584507678720/fbe610906bd63f2aceb3afd29d28bdee.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/15641825",
         github: "https://github.com/GalladeGuy",
     },
     {
         name: "King",
-        avatar: "https://cdn.discordapp.com/guilds/545842171459272705/users/707984911616704604/avatars/34cb914f7199528e3c1a53f77ceaa5c8.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/73195869",
         github: "https://github.com/MrKing202981",
     },
     {
         name: "WillFaustCuber",
-        avatar: "https://cdn.discordapp.com/avatars/451407331234807830/3056ed1f40d915b7e9c5587960fa5c51.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/39139570",
         github: "https://github.com/willfaust",
     },
     {
         name: "Ivar",
-        avatar: "https://cdn.discordapp.com/avatars/290398986085597184/871fe950051d98b88195cfe164fcf1b3.webp?size=100",
+        avatar: "https://avatars.githubusercontent.com/u/41924494",
         github: "https://github.com/IvarWithoutBones",
     },
 ];
